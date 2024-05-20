1e270e40785e7d6b8f6e0baa82b1c3d9efde889a
JaeJun Lee <40487895+Jae-Jun-Lee@users.noreply.github.com>
2024-05-20 02:56:13 +0000

    deploy demo app

---
+++ demos/rag-demos/question-answering/deploy-app/dockerfiles/src/app.py
@@ -0,0 +1,240 @@
+import json
+import requests
+
+import os
+import subprocess
+
+import gradio as gr
+from theme import EzmeralTheme
+#from ezm import *
+import ezm
+
+
+DOMAIN_NAME = "svc.cluster.local"
+NAMESPACE = open("/var/run/secrets/kubernetes.io/serviceaccount/namespace", "r").read()
+DEPLOYMENT_NAME = "llm"
+MODEL_NAME = DEPLOYMENT_NAME
+SVC = f"{DEPLOYMENT_NAME}-transformer.{NAMESPACE}.{DOMAIN_NAME}"
+URL = f"https://{SVC}/v1/models/{MODEL_NAME}:predict"
+
+HEADER = """
+    <div style='text-align: center;'>
+        <img src='/file=image/app-header.png' alt='ai-enabled-search' 
+             style='max-width: 100%; margin-left: auto; margin-right: auto;
+             height: auto;'>
+    </div>
+"""
+
+STYLE = """
+    <style>
+        .border-left-success {border-left: .25rem solid #1cc88a !important;padding-left: 5px;} 
+        .border-left-ready {border-left: .25rem solid #A6A6A6 !important;padding-left: 5px;}
+        .substep {margin-left: 50px;}
+        .mainstep {display: flex;}
+
+        .state.success { background: #01A982;}
+        .state.ready { background: #ddd;}
+        .state { margin-right: 5px;height: 14px;min-width: 14px;line-height: 14px;display: inline-block;color: #000;border-radius: 20px;text-indent: -9999px;font-size: .7em;text-shadow: 1px 1px 0 #fff;text-align: center;padding-right: 2px;box-shadow: inset 0 0 0 #000;}
+
+         .blink { animation: blinker 1.5s linear infinite; } @keyframes blinker { 50% {opacity: 0;}}
+    </style>
+"""
+
+CUSTOM_JS = """
+
+<script>
+function progress(lastIdex) {
+
+    document.querySelector("#btn_interface").click()
+    var strProgress = document.querySelector("#tb_interface > * > textarea").value
+    var listProgress = strProgress.split("\\n")
+    
+    var pointerIndex = lastIdex;
+
+    for (i = lastIdex; i < listProgress.length; i++) {
+        var line = listProgress[i].trim();
+        if(line === null || line === "")
+            continue;
+        pointerIndex++;
+        
+        var lineSplit = line.split(";")
+        
+        var elemDiv = document.querySelector("#"+lineSplit[0]);
+        if(elemDiv === null)
+            continue;
+             
+        if(elemDiv.classList.contains("mainstep")) {
+            elemState = elemDiv.querySelector("span");
+            if(lineSplit[1] === "OK") {
+                elemState.classList.remove("ready");
+                elemState.classList.add("success");
+                elemDiv.classList.remove("blink");
+            } else if(lineSplit[1] === "RUNNING") {
+                elemDiv.classList.add("blink");
+            }
+
+        } else if( elemDiv.classList.contains("substep")) {
+            elemDiv.classList.remove("border-left-ready")
+            if(lineSplit[1] === "OK") {
+                elemDiv.classList.add("border-left-success")
+            }
+        }
+    } 
+    return pointerIndex;
+}
+
+// remove async 
+async function pollProgress () { 
+  const timer = ms => new Promise(res => setTimeout(res, ms))
+  await timer(3000);
+  var lastIdex = 0;
+  for (var i = 0; i < 3600; i++) {
+    lastIdex = progress(lastIdex);
+    await timer(1000); 
+  }
+  return "";
+
+//window.onload = function(){
+pollProgress();
+//}
+</script>
+"""
+
+## config ##
+
+ezm.domain = 'ua.ezm.host'
+ezm.username = NAMESPACE
+#ezm.username = 'leejj881229'
+ezm.password = 'hpinvent!23'
+
+ezm.predictor_image = 'marketplace.us1.greenlake-hpe.com/ezmeral/ezkf/qna-vectorstore:v1.3.0-e658264'
+ezm.llm_image = 'marketplace.us1.greenlake-hpe.com/ezmeral/ezkf/qna-llm:v1.3.0-e658264'
+ezm.transformer_image = 'marketplace.us1.greenlake-hpe.com/ezmeral/ezkf/qna-transformer:v1.3.0-f7315d3'
+
+
+
+progress = ""
+IS_RUNNING = False
+
+def deploy():
+    global IS_RUNNING
+    if(IS_RUNNING):
+        print("Deployment is already underway.")
+        return
+    else:
+        IS_RUNNING = True
+
+    print("Deploy")
+    set_config()
+    ezm.config_environ()
+
+    #ezm.get_token()
+    create_vectorstore()
+    serve_vectorstore()
+    serve_llm()
+
+    IS_RUNNING = False
+
+def set_config():
+    ezm.domain = input_domain.value
+    ezm.username = input_username.value
+    ezm.password = input_password.value
+
+    ezm.predictor_image = input_predictor_image.value
+    ezm.llm_image = input_llm_image.value
+    ezm.transformer_image = input_transformer_image.value
+
+def log_progress(pro):
+    global progress
+    progress+= pro + "\n"
+
+
+def create_vectorstore():
+    log_progress("cvs;RUNNING")
+
+    docs = ezm.load_docs("documents")
+    log_progress("cvs_loading;OK")
+
+    texts = ezm.process_docs(docs, chunk_size=500, chunk_overlap=0)
+    log_progress("cvs_chunking;OK")
+
+    embeddings = ezm.hugging_face_embeddings()
+    log_progress("cvs_embedding;OK")
+
+    db = ezm.storing_chroma(texts, embeddings)
+    log_progress("cvs_storing;OK")
+
+    log_progress("cvs;OK")
+
+def serve_vectorstore():
+    log_progress("svs;RUNNING")
+
+    uri = ezm.logging_vector_store_artifact()
+    log_progress("svs_logging_vs;OK")
+
+    ezm.apply_vectorstore(uri)
+    log_progress("svs_creating_service;OK")
+
+    log_progress("svs;OK")
+
+def serve_llm():
+    log_progress("sll;RUNNING")
+
+    ezm.apply_llm()
+    log_progress("sll_creating_service;OK")
+
+    log_progress("sll;OK")
+
+
+def read_progress_call_by_client():
+    return progress
+
+def clean():
+    ezm.delete_object("inferenceservices", "llm")
+    ezm.delete_object("inferenceservices", "vectorstore")
+
+if __name__ == "__main__":
+    with gr.Blocks(theme=EzmeralTheme(), head=CUSTOM_JS) as app:
+        gr.HTML(STYLE)
+        # Application Header
+        gr.HTML(HEADER)
+
+
+        with gr.Accordion("General Info", open=False):
+            input_domain = gr.Textbox(label="domain", value=ezm.domain)
+            input_username = gr.Textbox(label="username", value=ezm.username)
+            input_password = gr.Textbox(label="password", value=ezm.password)
+            input_predictor_image = gr.Textbox(label="predictor image", value=ezm.predictor_image)
+            input_llm_image = gr.Textbox(label="llm image", value=ezm.llm_image)
+            input_transformer_image = gr.Textbox(label="transformer image", value=ezm.transformer_image)
+
+        # interface
+        tb_interface = gr.Textbox(value="", elem_id="tb_interface", visible=False)
+        btn_interface = gr.Button(value="", elem_id="btn_interface", visible=False)
+        btn_interface.click(fn=read_progress_call_by_client, outputs=tb_interface)
+
+        btn_deploy = gr.Button("Deploy")
+        #btn_deploy.click(fn=deploy, js="pollProgress")
+        btn_deploy.click(fn=deploy)
+
+        lb_create_vs = gr.HTML("<div id=\"cvs\" class=\"mainstep\"><span class=\"state ready\"></span>01.create-vectorstore</div>")
+        lb_create_vs_load_doc = gr.HTML("<div id=\"cvs_loading\" class=\"border-left-ready substep\">Load the Documents</div>")
+        lb_create_vs_process_doc = gr.HTML("<div id=\"cvs_chunking\" class=\"border-left-ready substep\">Document Processing: Chunking Text for the Language Model</div>")
+        lb_create_vs_embed = gr.HTML("<div id=\"cvs_embedding\" class=\"border-left-ready substep\">Generating Embeddings</div>")
+        lb_create_vs_store = gr.HTML("<div id=\"cvs_storing\" class=\"border-left-ready substep\">Storing them in Chroma</div>")
+
+
+        gr.HTML("<div id=\"svs\" class=\"mainstep\"> <span class=\"state ready\"></span>02.serve-vectorstore</div>")
+        gr.HTML("<div id=\"svs_logging_vs\" class=\"border-left-ready substep\">Logging the Vector Store as an Artifact</div>")
+        gr.HTML("<div id=\"svs_creating_service\" class=\"border-left-ready substep\">Creating the Inference Service</div>")
+
+
+        gr.HTML("<div id=\"sll\" class=\"mainstep\"> <span class=\"state ready\"></span>03.serve-llm</div>")
+        gr.HTML("<div id=\"sll_creating_service\" class=\"border-left-ready substep\">Creating the Inference Service</div>")
+
+
+        # Debug
+        btn_clean = gr.Button("Clean", elem_id="clean", visible=False)
+        btn_clean.click(fn=clean)
+
+    app.launch(server_name="0.0.0.0", server_port=8081, allowed_paths=["image/app-header.png"])
---
+++ demos/rag-demos/question-answering/deploy-app/dockerfiles/src/ezm.py
@@ -0,0 +1,263 @@
+    loader = TextLoader(fn)
+import os
+import glob
+import base64
+import requests
+import subprocess
+import mlflow
+import time
+
+from tqdm import tqdm
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import TextLoader
+from langchain_community.vectorstores import Chroma
+
+domain = None
+username = None
+password = None
+predictor_image = None
+llm_image = None
+transformer_image = None
+
+
+def config_environ():
+    os.environ["HTTP_PROXY"] = ""
+    os.environ["HTTPS_PROXY"] = ""
+    os.environ["NO_PROXY"] = "10.96.0.0/12, .local"
+
+    os.environ['MLFLOW_TRACKING_TOKEN'] = get_token()
+    os.environ["AWS_ACCESS_KEY_ID"] = os.environ['MLFLOW_TRACKING_TOKEN']
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "s3"
+    os.environ["AWS_ENDPOINT_URL"] = 'http://local-s3-service.ezdata-system.svc.cluster.local:30000'
+    os.environ["MLFLOW_S3_ENDPOINT_URL"] = os.environ["AWS_ENDPOINT_URL"]
+    os.environ["MLFLOW_S3_IGNORE_TLS"] = "true"
+    os.environ["MLFLOW_TRACKING_INSECURE_TLS"] = "true"
+    os.environ["MLFLOW_TRACKING_URI"] = "http://mlflow.mlflow.svc.cluster.local:5000"
+
+def get_token():
+    token_url = f"https://keycloak.{domain}/realms/UA/protocol/openid-connect/token"
+    data = {
+        "username" : username,
+        "password" : password,
+        "grant_type" : "password",
+        "client_id" : "ua-grant",
+    }   
+    token_responce = requests.post(token_url, data=data, allow_redirects=True, verify=False)
+    token = token_responce.json()["access_token"]
+    print("TOKEN: {token}")
+    return token
+
+def check_object_ready(obj, name):
+    result = subprocess.run(["kubectl", "get", obj+"/"+name, "-n", username],  capture_output=True)
+    output = result.stdout.decode("utf-8")
+    
+    
+    #print(result)
+    #print(output)
+
+    if not name in output:
+        return False
+    if "True" in output.split("\n")[1]:
+        return True
+
+def wait_for_ready(obj, name): # 5 * 12 * 10 = 10M
+    complete = False
+    for i in range(12 * 10):
+        complete = check_object_ready(obj, name)
+        if complete:
+            break
+        time.sleep(5)
+
+    if(complete):
+        print("K8S Object Ready: {name}")
+    else:
+        print("K8S Object Timeout: {name}")
+
+def delete_object(obj, name):
+    subprocess.run(["kubectl", "delete", obj+"/"+name, "-n", username])
+
+
+## 01.create-vectorstore ##
+def load_doc(fn):
+    loader = TextLoader(fn)
+    doc = loader.load()
+    return doc
+
+def load_docs(source_dir: str) -> list:
+    """Load all documents in a the given directory."""
+    fns = glob.glob(os.path.join(source_dir, "*.txt"))
+    docs = []
+    for i, fn in enumerate(tqdm(fns, desc="Loading documents...")):
+        docs.extend(load_doc(fn))
+
+    return docs
+
+def process_docs(docs: list, chunk_size: int, chunk_overlap: int) -> list:
+    """Load the documents and split them into chunks."""
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+    texts = text_splitter.split_documents(docs)
+    return texts
+
+def hugging_face_embeddings():
+    embeddings_model = "all-MiniLM-L6-v2"
+    return HuggingFaceEmbeddings(model_name=embeddings_model)
+    
+def storing_chroma(texts, embeddings):
+    db = Chroma.from_documents(texts, embeddings, persist_directory=f"{os.getcwd()}/db")
+    db.persist()
+    return db
+    
+def test_query(db):
+    query = "How can I create a cgroup?"
+    matches = db.similarity_search(query); matches
+    print(matches)
+
+## 02.serve-vectorstore ##
+def encode_base64(message: str):
+    encoded_bytes = base64.b64encode(message.encode('ASCII'))
+    return encoded_bytes.decode('ASCII')
+
+def get_or_create_experiment(exp_name):
+    """Register an experiment in MLFlow.
+    
+    args:
+      exp_name (str): The name of the experiment.
+    """
+    try:
+        mlflow.set_experiment(exp_name)
+    except Exception as e:
+        raise RuntimeError(f"Failed to set the experiment: {e}")
+
+def logging_vector_store_artifact():
+    # Create a new MLFlow experiment or re-use an existing one
+    get_or_create_experiment('question-answering')
+    # Log the Chroma DB files as an artifact of the experiment
+    print(f"{os.getcwd()}/db")
+    mlflow.log_artifact(f"{os.getcwd()}/db")
+    # Retrieve the URI of the artifact
+    uri = mlflow.get_artifact_uri("db")
+    return uri
+
+def apply_vectorstore(uri):
+    isvc = """
+apiVersion: v1
+kind: Secret
+metadata:
+  name: minio-secret
+type: Opaque
+data:
+  MINIO_ACCESS_KEY: {0}
+  MINIO_SECRET_KEY: {1}
+
+---
+apiVersion: serving.kserve.io/v1beta1
+kind: InferenceService
+metadata:
+  name: vectorstore
+spec:
+  predictor:
+    containers:
+    - name: kserve-container
+      image: {2}
+      imagePullPolicy: Always
+      resources:
+        requests:
+          memory: "2Gi"
+          cpu: "500m"
+        limits:
+          memory: "2Gi"
+          cpu: "500m"
+      args:
+      - --persist-uri
+      - {3}
+      env:
+      # If you are running behind a proxy, uncomment the following lines and replace the values with your proxy URLs.
+      - name: HTTP_PROXY
+        value: {4}
+      - name: HTTPS_PROXY
+        value: {5}
+      - name: NO_PROXY
+        value: .local
+      - name: MLFLOW_S3_ENDPOINT_URL
+        value: {6}
+      - name: TRANSFORMERS_CACHE
+        value: /src
+      - name: SENTENCE_TRANSFORMERS_HOME
+        value: /src
+      - name: MINIO_ACCESS_KEY
+        valueFrom:
+          secretKeyRef:
+            key: MINIO_ACCESS_KEY
+            name: minio-secret
+      - name: MINIO_SECRET_KEY
+        valueFrom:
+          secretKeyRef:
+            key: MINIO_SECRET_KEY
+            name: minio-secret
+""".format(encode_base64(os.environ["AWS_ACCESS_KEY_ID"]),
+           encode_base64(os.environ["AWS_SECRET_ACCESS_KEY"]),
+           predictor_image,
+           uri,
+           os.environ["HTTP_PROXY"],
+           os.environ["HTTPS_PROXY"],
+           os.environ["MLFLOW_S3_ENDPOINT_URL"])
+    with open("vectorstore-isvc.yaml", "w") as f:
+        f.write(isvc)
+    
+    subprocess.run(["kubectl", "apply", "-f", "vectorstore-isvc.yaml"])
+
+    wait_for_ready("inferenceservice","vectorstore")
+
+## 04.serve-llm ##
+def apply_llm():
+    isvc = """
+apiVersion: serving.kserve.io/v1beta1
+kind: InferenceService
+metadata:
+  name: llm
+spec:
+  predictor:
+    timeout: 600
+    containers:
+      - name: kserve-container
+        image: {0}
+        imagePullPolicy: Always
+        # If you are running behind a proxy, uncomment the following lines and replace the values with your proxy URLs.
+        env:
+        - name: HTTP_PROXY
+          value: {1}
+        - name: HTTPS_PROXY
+          value: {2}
+        - name: NO_PROXY
+          value: .local
+        resources:
+          requests:
+            memory: "8Gi"
+            cpu: "1000m"
+          limits:
+            memory: "8Gi"
+            cpu: "1000m"
+  transformer:
+    timeout: 600
+    containers:
+      - image: {3}
+        imagePullPolicy: Always
+        resources:
+          requests:
+            memory: "1Gi"
+            cpu: "500m"
+          limits:
+            memory: "1Gi"
+            cpu: "500m"
+        name: kserve-container
+        args: ["--use_ssl"]
+""".format(llm_image,
+           os.environ["HTTP_PROXY"],
+           os.environ["HTTPS_PROXY"],
+           transformer_image)
+    with open("llm-isvc.yaml", "w") as f:
+        f.write(isvc)
+    subprocess.run(["kubectl", "apply", "-f", "llm-isvc.yaml"])
+
+    wait_for_ready("inferenceservice", "llm")
